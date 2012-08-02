f7b42415e5e8d7cadef612eca885f810938083cf
Shubham Dhamande <sdhamande@gmail.com>
2012-08-02 21:59:16 +0530

    updating port

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ var app = require('express').createServer();
 app.get('/*', function(req, res) {
     res.sendfile(__dirname + req.path);
 });
-app.listen(process.env.VCAP_APP_PORT || 3000);
+app.listen(8080);
