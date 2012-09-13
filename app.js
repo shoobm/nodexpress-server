e09affad8ee3c447ba03a6421fc819820907d6cb
Shubham Dhamande <sdhamande@gmail.com>
2012-09-13 18:07:58 +0530

    error handling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = require('express').createServer();
 app.get('/*', function(req, res) {
-    res.sendfile(__dirname + req.path);
+    res.sendfile(__dirname + req.path, function(err){
+	res.send(404, 'Not Found');
+  });
 });
-app.listen(8080);
+app.listen(80);
